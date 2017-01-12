17ef773b9493891903ff62e21bc691e6f5208082
Jared Laser <laserjn@gmail.com>
2017-01-12 16:25:47 -0500

    tidy up spacing

--- src/redux/ducks/index.js
+++ src/redux/ducks/index.js
@@ -1,9 +1,5 @@
 import { combineReducers } from 'redux';
 
-// import auth from './auth.js';
-// import pledge from './pledge.js';
-// import counters from './counters.js';
-
 const rootReducer = combineReducers({
 });
 
